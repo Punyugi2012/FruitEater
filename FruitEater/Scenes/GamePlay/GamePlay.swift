f7183b22f14a44425909d39088fb875231c55107
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-09 16:08:40 +0700

    added scenes

---
+++ FruitEater/Scenes/GamePlay/GamePlay.swift
@@ -0,0 +1,14 @@
+//
+//  GamePlay.swift
+//  FruitEater
+//
+//  Created by punyawee  on 9/8/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GamePlay: SKScene {
+
+}
---
+++ FruitEater/Scenes/MainMenu/MainMenu.swift
@@ -0,0 +1,14 @@
+//
+//  MainMenu.swift
+//  FruitEater
+//
+//  Created by punyawee  on 9/8/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MainMenu: SKScene {
+
+}
